14754e331197ddb1ddebeb19c65ca4bc1ace8d84
김선우 <frod5@kakao.com>
2024-05-10 18:02:57 +0900

    자바중급편2 - 컬렉션 - 직접구현하는 배열 리스트3

---
+++ src/collection/array/MyArrayListV3.java
@@ -0,0 +1,97 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class MyArrayListV3 {
+	private static final int DEFAULT_CAPACITY = 5;
+
+	private Object[] elementData;
+	private int size = 0;
+
+	public MyArrayListV3() {
+		elementData = new Object[DEFAULT_CAPACITY];
+	}
+
+	public MyArrayListV3(int initialCapacity) {
+		elementData = new Object[initialCapacity];
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public void add(Object obj) {
+		//코드 추가
+		if(size == elementData.length) {
+			grow();
+		}
+		elementData[size] = obj;
+		size++;
+	}
+
+	public void add(int index, Object obj) {
+		//코드 추가
+		if(size == elementData.length) {
+			grow();
+		}
+
+		//데이터 이동
+		shiftRightFrom(index);
+		elementData[index] = obj;
+		size++;
+	}
+
+	//코드추가, 요소의 마지막부터 index까지 오른쪽으로 밀기
+	private void shiftRightFrom(int index) {
+		for (int i = size; i > index; i--) {
+			elementData[i] = elementData[i-1];
+		}
+	}
+
+	private void grow() {
+		//배열을 새로 만들고, 기존배열을 새로운 배열에 복사
+		elementData = Arrays.copyOf(elementData, elementData.length * 2);
+	}
+
+	public Object get(int index) {
+		return elementData[index];
+	}
+
+	public Object set(int index, Object obj) {
+		Object oldData = get(index);
+		elementData[index] = obj;
+		return oldData;
+	}
+
+	public Object remove(int index) {
+		Object oldValue = get(index);
+		//데이터이동
+		shiftLeftFrom(index);
+
+		size--;
+		elementData[size] = null;
+		return oldValue;
+	}
+
+	//코드 추가 요소의 index부터 마지막까지 왼쪽으로 밀기.
+	private void shiftLeftFrom(int index) {
+		for (int i = index; i < size - 1; i++) {
+			elementData[i] = elementData[i+1];
+		}
+	}
+
+	public int indexOf(Object obj) {
+		for (int i = 0; i < size; i++) {
+			if(elementData[i].equals(obj)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.toString(Arrays.copyOf(elementData, size)) + " size= " +
+			size + " ,capacity=" + elementData.length;
+	}
+}
