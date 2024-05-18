01780ef8f8b7fcf10dc77c3a2988dbec7a289a06
김선우 <frod5@kakao.com>
2024-05-18 18:38:54 +0900

    자바중급편2 - 컬렉션 - 향상된 for문

--- src/collection/iterable/MyArrayMain.java
+++ src/collection/iterable/MyArrayMain.java
@@ -13,5 +13,13 @@ public class MyArrayMain {
             Integer next = iterator.next();
             System.out.println("next = " + next);
         }
+
+        //추가
+        //향상된 for문
+        //Iterable을 구현해야 사용할 수 있다.
+        System.out.println("for-each 사용");
+        for (Integer next : myArray) {
+            System.out.println("next = " + next);
+        }
     }
 }
